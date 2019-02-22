6357c8ce26d1aca485fea34bd7caaf843ca833b8
lourod1987 <louart87@gmail.com>
2019-02-22 03:33:20 -0600

    moving asteroidgrid.vue into appropriate folder. Completing setup for single file vue components of near earth objects table

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,42 @@
 </template>
 
 <script>
-import AsteroidGrid from './component/AsteroidGrid.vue'
+import AsteroidGrid from './components/AsteroidGrid.vue'
 
 export default {
   name: 'app',
   components: {
     AsteroidGrid
-  }
+  },
+  data() {
+    return {
+      asteroids: []
+    }
+  },            
+  created: function () {
+    this.fetchAsteroids();
+  },
+  methods: {
+    fetchAsteroids:
+      function () {
+        var apiKey = 'qtbDAzGCDR7zrNAypKP7VrE3zxUyL76W9nFsp1at';
+        var url = `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${apiKey}`;
+        fetch(url)
+        .then((res) => res.json())
+        .then((val) => {
+          this.asteroids = val.near_earth_objects.slice(10, 20);
+          });
+        },
+      remove: 
+        function (index) {
+          this.asteroids.splice(index, 1);
+        }
+    }
 }
 </script>
 
 <style>
-
+  [v-cloak] {
+    display: none;
+  }
 </style>
